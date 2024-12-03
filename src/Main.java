044bc7af07a435994a1e61c4c949024fc2249593
ПристрійUNICEF <karisha.vakula@gmail.com>
2024-12-03 16:50:47 +0200

    init comit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//import java.awt.*;
+
+import figures.Rectangle;
+
+public class Main {
+  public static void main(String[] args) {
+      Rectangle rectangle = new Rectangle();
+      int square = rectangle.getSquare();
+
+
+
+
+  }
+
+
+
+
+
+}
+
+
+
+
---
+++ src/figures/Rectangle.java
@@ -0,0 +1,30 @@
+package figures;
+
+public class Rectangle {
+
+    int a;
+    int b;
+
+    public Rectangle(int storona1, int storona2) {
+
+      a = storona1;
+      b = storona2;
+
+    }
+
+    public Rectangle() {
+
+    }
+
+    public int getSquare() {
+
+        Rectangle square = new Rectangle(10, 4);
+        System.out.println("square.a *" "* square.b")
+
+
+        return 0;
+    }
+
+
+
+}
